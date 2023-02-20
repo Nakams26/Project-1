58798e6816975ca348c2eaca95d66a52551f60eb
adrienpolselli <adrienpolselli@hotmail.com>
2023-02-20 11:39:31 -0500

    Cleaning comments

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ const closeMenu = document.querySelector(".menuClose");
 //Add a event listener to open menu when the burger menu is clicked
 
 hamburgerMenu.addEventListener("click", function () {
-  // If when you click the burger button, the menu doesn't contain showMenu as class, then I add the class (The class allow us to display the menu form the right).  I also in the same time hide the menu open and display the close menu
+  // If when you click the burger button, the menu doesn't contain showMenu as class, then I add the class (The class allow us to display the menu from the right with a transform translate X).  I also in the same time hide the open icon and display the close icon
   if (!dynamicLi.classList.contains("showMenu")) {
     dynamicLi.classList.add("showMenu");
     closeMenu.style.display = "block";
     openMenu.style.display = "none";
   }
-  //   // If when you click the burger button, the menu  contain showMenu as class, then I remove the class. This will hide the menu
+  // If when you click the burger button, the menu  already contain showMenu as class, then I remove the class. This will hide the menu
   else {
     dynamicLi.classList.remove("showMenu");
     //I also in the same time show the  open menu icon and hide the close menu
